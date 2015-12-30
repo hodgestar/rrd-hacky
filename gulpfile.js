3585c29b8684e5b52ec626ded03001e774b2f1f6
Simon Cross <hodgestar@gmail.com>
2015-12-30 13:57:40 +0200

    Initial gulp build command.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+    sourceMaps = require('gulp-sourcemaps'),
+    typescript = require('gulp-typescript'),
+    babel = require('gulp-babel');
+
+var srcPath = 'src/**.ts*';
+var jsBuildPath = 'js';
+
+gulp.task('build', function () {
+  return gulp.src([srcPath, 'typings/tsd.d.ts'])
+    .pipe(sourceMaps.init())
+    .pipe(typescript('tsconfig.json'))
+    .pipe(babel({presets: "es2015"}));
+    .pipe(sourceMaps.write())
+    .pipe(gulp.dest(jsBuildPath));
+});
