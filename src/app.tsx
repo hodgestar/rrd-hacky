fda4eddc05c6b92eedb4a22dbc941a7ca97296c3
Simon Cross <hodgestar@gmail.com>
2015-12-30 16:22:24 +0200

    Add sample data.

--- src/app.tsx
+++ src/app.tsx
@@ -2,29 +2,40 @@ import {Diagram} from './diagram';
 import * as React  from 'react';
 import * as ReactDOM from 'react-dom';
 
-var sampleData = [];
+var sampleData = [
+    {
+        title: "Node 1",
+        w: 50, h: 50,
+        x: 50, y: 50,
+    },
+    {
+        title: "Node 2",
+        w: 50, h: 50,
+        x: 50, y: 50,
+    },
+];
 
 interface IAppState {
     data: any[];
-    
+
 }
 
 export class App extends React.Component<IAppState, IAppState> {
-    
+
     constructor(props) {
         super(props);
     }
-    
+
     public state: IAppState = {
         data: sampleData
     };
-    
+
     public render() {
         return (<div>
             <Diagram data={ this.state.data } />
         </div>);
     }
-    
+
 }
 
 ReactDOM.render(React.createElement(App, null), document.getElementById("react-root"));
